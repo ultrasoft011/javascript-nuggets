a533e0c69096c6a82ef6659731a0c427c68990b5
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-26 17:47:45 -0500

    Array of objects

---
+++ index.js
@@ -0,0 +1,21 @@
+// Array of objetcs
+const frameworks = [
+    {
+        id: 1,
+        name: "Vue",
+        creator: "Evan You",
+        last_update: 3.011        
+    },
+    {
+        id: 2,
+        name: "Angular",
+        creator: "Miško Hevery",
+        last_update: 12.0
+    },
+    {
+        id: 3,
+        name: "React",
+        creator: "Jordan Walke",
+        last_update: 17.2
+    }
+]
