a7fdb35282afdf47436c513f62305cee1f6bb822
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-09 11:30:40 -0500

    callback hell fundamentals

--- callback.js
+++ callback.js
@@ -36,3 +36,23 @@ handleName('aka Ultra', (value) => {console.log(value)})
     example.addEventListener('click', function() {
         console.log('Hello Ultra!');
     })
+
+
+// Javascript: Callback Hell
+
+const first = document.querySelector('.first');
+const second = document.querySelector('.second');
+const third = document.querySelector('.third');
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        first.style.color = 'yellow';
+    }, 2000);
+    setTimeout(() => {
+        second.style.color = 'blue';
+    }, 3000)
+    setTimeout(() => {
+        third.style.color = 'red';
+    }, 4000);
+});
