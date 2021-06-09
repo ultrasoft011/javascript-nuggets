2b0e682b09caa9dedfd207e727e662b674c782e5
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-09 10:21:44 -0500

    callback function examples addEventListener

--- callback.js
+++ callback.js
@@ -17,3 +17,22 @@ function handleName(name, cb) {
 // 3. Invoke the function with parameters, the second parameter (makeUpperCase) dont need "()" because if I invoke the function that will be a reference and not passed like a callback function.
 
 handleName('Andres', makeUpperCase);
+
+// Option B: The callback function can be pass directly but not invoked
+
+handleName('Felipe', function(value) {
+    console.log(value);
+})
+
+// Option B.1 Using arrow functions
+
+handleName('aka Ultra', (value) => {console.log(value)})
+
+// The callback functions are use in arrays methods (map, filter, find, reduce) , setTimeOut, Event Listeners
+// Event listener example: the function inside the addEventListener function is a callback function
+
+    const example = document.querySelector('.btn');
+
+    example.addEventListener('click', function() {
+        console.log('Hello Ultra!');
+    })
