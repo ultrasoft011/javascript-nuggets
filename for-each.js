d426ee4c517db21e16bdc15ab0fd91a10a31b459
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-13 19:57:11 -0500

    forEach

---
+++ for-each.js
@@ -0,0 +1,7 @@
+// The forEach() method executes a provided function once for each array element.
+
+const array1 = ["a", "b", "c"];
+
+array1.forEach((element) => console.log(element));
+
+// forEach() may be preferable when you’re not trying to change the data in your array, but instead want to just do something with it — like saving it to a database or logging it out:
