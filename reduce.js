288fe9dc6284b03a060cb8b5b31ced19429b158d
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-13 19:39:24 -0500

    reduce basics

--- reduce.js
+++ reduce.js
@@ -8,12 +8,14 @@ const staff = [
     {name: 'junior', category: 1, salary: 4000000},
     {name: 'junior adv', category: 2, salary: 5350000},
     {name: 'senior', category: 3, salary: 7000000}
-]
+];
 
 // Declare a new variable
 
 const devSalary = staff.reduce((total, item) => {
-
+    total += item.salary;
     // Important: always return a total or return whatever it's the name of the first parameter
     return total
-} )
+},0) /* Important: the second argument it will be the start of the iteration */
+
+console.log(devSalary);
